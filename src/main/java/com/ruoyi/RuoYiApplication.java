6c513a069ba43750ceb2f9313e380518bc96d4a2
liulu <978768883@qq.com>
2020-12-28 15:38:44 +0800

    合并分支

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RuoYiApplication
+
 {
     public static void main(String[] args)
     {
